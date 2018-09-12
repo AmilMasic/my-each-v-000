dabad0d57425fc291b5289c9073df43b5d8fd0eb
Amil Masic <amil.masic@gmail.com>
2018-09-12 02:48:08 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(collection) # put argument(s) here
   # code here
   # if block_given?
     i = 0
-    while i < collection.lenght
+    while i < collection.length
       yield collection[i]
       i +=1
     # else
